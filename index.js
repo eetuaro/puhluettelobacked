b04e482b243f9caa8ec1b7a9f315ecef31b99a4a
eetuaro <124339857+eetuaro@users.noreply.github.com>
2024-08-15 19:20:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson);
 });
 
-const PORT = process.env.Port || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
