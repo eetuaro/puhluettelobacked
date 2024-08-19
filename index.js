321132ad1f32ebb16b00a7d9e9a6f24fd16505b0
eetuaro <124339857+eetuaro@users.noreply.github.com>
2024-08-19 22:00:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body);
@@ -83,3 +84,4 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
